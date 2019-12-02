bd2d663c15af3d641a000c620e77eb0c61738b23
IK <ikthreeo@gmail.com>
2019-12-02 01:02:46 -0500

    14. Functions - Parameters and Arguments

--- playground/custom-functions/cf-FINISHED.js
+++ playground/custom-functions/cf-FINISHED.js
@@ -1,23 +1,22 @@
 // Function Definition
 function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
   // this is the function body
-  console.log(`Running Calculate Bill!!`);
+  console.log('Running Calculate Bill!!');
   const total = billAmount + billAmount * taxRate + billAmount * tipRate;
   return total;
 }
 
 // Function Call. Or **Run**
-// const myTotal = calculateBill(100);
-// const myBill2 = calculateBill(100, undefined, 0.2);
-// console.log(myTotal);
+const wesTotal = 500;
+const wesTaxRate = 0.3;
+// const myTotal = calculateBill(wesTotal, wesTaxRate);
 
 // Function Definition
 function sayHiTo(firstName) {
   return `Hello ${firstName}`;
 }
 
-// Function Call. Or **Run**
-// const greeting = sayHiTo(`IK`);
+// const greeting = sayHiTo('Wes');
 // console.log(greeting);
 
 function doctorize(name) {
@@ -27,3 +26,6 @@ function doctorize(name) {
 function yell(name = 'Silly Goose') {
   return `HEY ${name.toUpperCase()}`;
 }
+
+const myBill4 = calculateBill(100, undefined, 0.2);
+console.log(myBill4);
